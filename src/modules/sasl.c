23f6c7e2893e5e011b5c24fc545ec0bf6e03f647
Celelibi <celelibi@gmail.com>
2025-02-24 08:30:15 +0100

    Fix SASL issue?
    This made-up commit comes from the source archive.
    
    Signed-off-by: Celelibi <celelibi@gmail.com>

--- src/modules/sasl.c
+++ src/modules/sasl.c
@@ -25,7 +25,7 @@
 ModuleHeader MOD_HEADER
   = {
 	"sasl",
-	"5.0",
+	"5.2.0",
 	"SASL", 
 	"UnrealIRCd Team",
 	"unrealircd-5",
@@ -396,6 +396,7 @@ MOD_INIT()
 	mreq.serialize = saslmechlist_serialize;
 	mreq.unserialize = saslmechlist_unserialize;
 	mreq.sync = 1;
+	mreq.remote_write = 1;
 	mreq.type = MODDATATYPE_CLIENT;
 	ModDataAdd(modinfo->handle, mreq);
 
